945cc5f4f5ced86fea7792055d4ff6a8f1a263a7
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2025-02-05 22:00:17 -0600

    feat(ui): reactions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,17 @@ class HomeController < ApplicationController
 
   ELEMENTS_PER_PAGE = 10
 
+  skip_before_action :authenticate_user!, only: [:login]
+  before_action :find_billboard, only: [:upvote, :downvote]
+
+  def login
+    render "layouts/login"
+  end
+
   def index
     @per_page = ELEMENTS_PER_PAGE
     @page = params[:page].to_i > 0 ? params[:page].to_i : 1
     @total_billboards = Billboard.count
-    @billboards = Billboard.limit(ELEMENTS_PER_PAGE).offset((@page - 1) * ELEMENTS_PER_PAGE)
 
     case params[:filter]
     when "trending"
@@ -19,7 +25,15 @@ class HomeController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.js
+      format.json do 
+        render json: { 
+          html: render_to_string(
+            partial: "home/list", 
+            formats: [:html], 
+            locals: { billboards: @billboards, page: @page }
+          ) 
+        }
+      end
     end
   end
 
@@ -32,13 +46,33 @@ class HomeController < ApplicationController
     )
     if @csv_upload.save
       ProcessCsvJob.perform_later(@csv_upload.id)
-      redirect_to root_path, notice: "CSV file uploaded successfully! Processing in the background."
+      redirect_to home_index_path, notice: "CSV file uploaded successfully! Processing in the background."
     else
       render :new
     end
   end
 
+  def upvote
+    @billboard.increment(current_user)
+
+    respond_to do |format|
+      format.json { render json: { likes: @billboard.likes, dislikes: @billboard.dislikes } }
+    end
+  end
+
+  def downvote
+    @billboard.decrement(current_user)
+    
+    respond_to do |format|
+      format.json { render json: { likes: @billboard.likes, dislikes: @billboard.dislikes } }
+    end
+  end
+  
   private
+  
+  def find_billboard
+    @billboard = Billboard.find(params[:id])
+  end
 
   def csv_upload_params
     params.require(:csv_upload).permit(:title, :file)
--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -20,4 +20,24 @@ class Billboard < ApplicationRecord
   def dislikes
     billboard_interactions.where(reaction: -1).count
   end
+
+  def increment(user)
+    positive_interaction = billboard_interactions.find_by(user: user, reaction: 1)
+    return if positive_interaction.present?
+
+    negative_interaction = billboard_interactions.find_by(user: user, reaction: -1)
+    return negative_interaction.destroy if negative_interaction.present?
+
+    billboard_interactions.create(user: user, reaction: 1)
+  end
+
+  def decrement(user)
+    negative_interaction = billboard_interactions.find_by(user: user, reaction: -1)
+    return if negative_interaction.present?
+
+    positive_interaction = billboard_interactions.find_by(user: user, reaction: 1)
+    return positive_interaction.destroy if positive_interaction.present?
+
+    billboard_interactions.create(user: user, reaction: -1)
+  end
 end
